d32d1a6ed37335929a7ea7e642c3614cc4992d86
Jérôme Carrot <jerome.carrot+git@gmail.com>
2017-10-16 11:15:42 -0500

    Add apache image and composer env for magento 2

---
+++ compose-magento2/reset-permissions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USER_NAME=$1
+GROUP_NAME=$2
+WRITABLE_FOLDERS="www/app/etc www/var www/pub/media www/pub/static www/generated"
+
+if [ -z "$GROUP_NAME" ]; then
+	GROUP_NAME=www-data
+fi
+
+# Reset standard permissions on all directories and files
+if [ ! -z "$USER_NAME" ]; then
+    chown -R $USER_NAME:$USER_NAME www
+fi
+find www -type d -exec chmod 755 {} \;
+find www -type f -exec chmod 644 {} \;
+
+# Set magento specific permissions
+chgrp -R $GROUP_NAME $WRITABLE_FOLDERS
+chmod -R g+w $WRITABLE_FOLDERS
+chmod u+x www/bin/magento
+
